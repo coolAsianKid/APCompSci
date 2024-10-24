006c8de70255cea752b2d823b77d8071e95517a2
coolAsianKid <jpark25@bfacademy.de>
2024-10-24 15:05:40 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,30 @@
 public class Hello{
     public static void main (String[] args){
-        System.out.println("Hello");
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+        int p4 = 0;
+        int p5 = 0;
+        int snum = 0;
+        if (p1 == p2 || p2 == p3 || p3 == p4 || p4 ==p5)
+            snum = snum +1;
+        else
+            System.out.println(0);
+        if (p1 == p2 || p2 == p3 || p3 == p4 || p4 ==p5)
+            snum = snum +1;
+        else
+            System.out.println(0);
+        if (p1 == p2 || p2 == p3 || p3 == p4 || p4 ==p5)
+            snum = snum +1;
+        else
+            System.out.println(0);
+        if (p1 == p2 || p2 == p3 || p3 == p4 || p4 ==p5)
+            snum = snum +1;
+        else
+            System.out.println(0);
+        if (p1 == p2 || p2 == p3 || p3 == p4 || p4 ==p5)
+            snum = snum +1;
+        else
+            System.out.println(0);
     }
 }
